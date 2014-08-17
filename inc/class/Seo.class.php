ff4680406482dab6192b3eb72890ecb694a13141
William Bruno <rocha_bruno@hotmail.com>
2014-08-17 11:49:59 -0300

    Fix SEO

--- inc/class/Seo.class.php
+++ inc/class/Seo.class.php
@@ -25,32 +25,38 @@ class Seo {
     switch( $pg )
     {
       case '404.html':
-        $arr['desc'] = 'Essa página não existe, mas você pode visitar outras. ';
+        $arr['desc'] = 'Essa página não existe, mas você pode visitar outras.';
         $arr['title'] = 'Esse projeto não foi criado ainda. | ' . self::$title;
-        $arr['canonical'] = self::getUrl() . '/404.html';
+        $arr['canonical'] = self::getUrl() . '/404';
         break;
 
-      case 'home':
+      case 'home.html':
         $arr['desc'] = self::$desc_default;
         $arr['title'] = self::$title;
         $arr['canonical'] = self::getUrl();
         break;
 
-      case 'projetos':
+      case 'projetos.html':
         $arr['desc'] = 'Projetos de Arquitetura do Studio AG. ' . self::$desc_default;
         $arr['title'] = 'Projetos de Arquitetura  | '.self::$title;
         $arr['canonical'] = self::getUrl(). '/projetos';
         break;
 
-      case 'feed':
+      case 'studio.html':
+        $arr['desc'] = 'Criado com a proposta de produzir uma arquitetura contemporânea. ' . self::$desc_default;
+        $arr['title'] = 'Amanda Castro e Giovana Giosa  | '.self::$title;
+        $arr['canonical'] = self::getUrl(). '/studio';
+        break;
+
+      case 'feed.html':
         $arr['desc'] = 'Confira nosso perfil no Pinterest. Usamos essa plataforma para arquivar referências e tendências da arquitetura para nossos projetos.';
-        $arr['title'] = 'Nosso Pinterest e Instagram | ';
+        $arr['title'] = 'Nosso Pinterest e Instagram | '.self::$title;
         $arr['canonical'] = self::getUrl() . '/feed';
         break;
 
-      case 'contato':
+      case 'contato.html':
         $arr['desc'] = 'Saiba como contratar nossos serviços. ' . self::$desc_default;
-        $arr['title'] = 'Entre em contato | ';
+        $arr['title'] = 'Entre em contato | '.self::$title;
         $arr['canonical'] = self::getUrl() . '/contato';
         break;
 
